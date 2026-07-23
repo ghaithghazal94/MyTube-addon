8f8b114d50ed357dba89483ef2d2ff8eb6ebb2de
ghaithghazal94 <ghaithghazal94@gmail.com>
2026-07-23 17:17:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 const express = require("express");
-const { getRouter } = require("stremio-addon-sdk");
-const addon = require("./addon");
 
 const app = express();
 
 const PORT = process.env.PORT || 7000;
 
-
-// اختبار بسيط
 app.get("/", (req, res) => {
-    res.status(200).send("MyTube Stremio Addon Running");
+    res.send("MyTube is alive");
 });
 
+app.get("/manifest.json", (req, res) => {
+    res.json({
+        id: "com.mytube.search",
+        version: "1.0.0",
+        name: "MyTube Test",
+        description: "Test addon",
+        resources: ["catalog"],
+        types: ["movie"],
+        catalogs: [
+            {
+                type: "movie",
+                id: "test",
+                name: "Test"
+            }
+        ]
+    });
+});
 
-// تشغيل Stremio addon
-app.use(getRouter(addon));
-
-
-// Railway يحتاج الاستماع على كل الواجهات
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`MyTube running on port ${PORT}`);
+    console.log("Server running on " + PORT);
 });
